2b4c40895f5757b52215c781de0254e489b15766
NedxLab <odoc047@gmail.com>
2023-02-22 18:13:35 +0100

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,4 @@
-import {
-  aboutLinks,
-  serviceLinks,
-  learnLinks,
-  productLinks,
-  supportLinks,
-} from "@/data/footerLinks";
+import { footerLinks } from "@/data/footerLinks";
 import {
   FaFacebook,
   FaPinterest,
@@ -12,52 +6,77 @@ import {
   FaTwitter,
   FaDiscord,
   FaMedium,
+  FaPlus,
+  FaMinus,
 } from "react-icons/fa";
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <>
-      <ul className="flex flex-row justify-evenly mt-10">
+      <ul className="flex flex-row justify-evenly mt-10 mmd:hidden">
+        {footerLinks.map((el, i) => (
+          <li className="font-bold py-2 msd:py-8">
+            <div className="flex flex-row justify-between">
+              <h1> {el.name}</h1>
+              <button className="hidden mmd:block">
+                <FaPlus />
+              </button>
+            </div>
+            <ul className="flex flex-col gap-1 font-light overflow-hidden mt-2">
+              {el.links.map((link, index) => (
+                <li key={index}>
+                  <Link href={link.slug}>{link.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+
         <li className="font-bold py-8">
-          About us{" "}
-          <ul className="flex flex-col justify-evenly font-light">
-            {aboutLinks.map((link, index) => (
-              <li key={index}>{link.name}</li>
-            ))}
+          Community{" "}
+          <ul className="flex flex-row space-x-4 flex-wrap  justify-evenly">
+            <FaDiscord />
+            <FaFacebook />
+            <FaTwitter /> <br />
+            <FaInstagram />
+            <FaMedium />
+            <FaPinterest />
           </ul>
         </li>
-        <li className="font-bold py-8">
-          Products{" "}
-          <ul className="flex flex-col justify-evenly font-light">
-            {productLinks.map((link, index) => (
-              <li key={index}>{link.name}</li>
-            ))}
-          </ul>
-        </li>
-        <li className="font-bold py-8">
-          Service{" "}
-          <ul className="flex flex-col justify-evenly font-light">
-            {serviceLinks.map((link, index) => (
-              <li key={index}>{link.name}</li>
-            ))}
-          </ul>
-        </li>
-        <li className="font-bold py-8">
-          Support{" "}
-          <ul className="flex flex-col justify-evenly font-light">
-            {supportLinks.map((link, index) => (
-              <li key={index}>{link.name}</li>
-            ))}
-          </ul>
-        </li>
-        <li className="font-bold py-8">
-          Learn{" "}
-          <ul className="flex flex-col justify-evenly font-light">
-            {learnLinks.map((link, index) => (
-              <li key={index}>{link.name}</li>
-            ))}
-          </ul>
-        </li>
+      </ul>
+
+      <ul className="hidden  flex-row justify-evenly mt-10 mmd:flex-col mmd:flex">
+        {footerLinks.map((el, i) => (
+          <Menu as="li" className="font-bold py-2 msd:py-8">
+            <div className="flex flex-row justify-between">
+              <h1> {el.name}</h1>
+              <Menu.Button className="hidden mmd:block">
+                <FaPlus />
+              </Menu.Button>
+            </div>
+            <Transition
+              as={Fragment}
+              enter="transition-h ease-out duration-500"
+              enterFrom="h-0"
+              enterTo="h-64"
+              leave="transition-h ease-in duration-500"
+              leaveFrom=" h-64"
+              leaveTo=" h-0"
+            >
+              <Menu.Items className="flex flex-col gap-1 font-light overflow-hidden mt-2">
+                {el.links.map((link, index) => (
+                  <Menu.Item key={index}>
+                    <Link href={link.slug}>{link.name}</Link>
+                  </Menu.Item>
+                ))}
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        ))}
+
         <li className="font-bold py-8">
           Community{" "}
           <ul className="flex flex-row space-x-4 flex-wrap  justify-evenly">
--- data/footerLinks.tsx
+++ data/footerLinks.tsx
@@ -1,43 +1,59 @@
-export const aboutLinks = [
-  { name: "About", slug: "/" },
-  { name: "Careers", slug: "/" },
-  { name: "Business Contacts", slug: "/" },
-  { name: "Community", slug: "/" },
-  { name: "Legal", slug: "/" },
-  { name: "Terms of Service", slug: "/" },
-  { name: "Privacy", slug: "/" },
-  { name: "Risks", slug: "/" },
-  { name: "Announcements", slug: "/" },
-  { name: "Notices", slug: "/" },
-];
-export const productLinks = [
-  { name: "Exchange", slug: "/" },
-  { name: "Academy", slug: "/" },
-  { name: "Napcat Live", slug: "/" },
-  { name: "Charity", slug: "/" },
-  { name: "Labs", slug: "/" },
-  { name: "Launchpad", slug: "/" },
-  { name: "Research", slug: "/" },
-];
-
-export const serviceLinks = [
-  { name: "Downloads", slug: "/" },
-  { name: "Desktop Application", slug: "/" },
-  { name: "Buy Crypto", slug: "/" },
-  { name: "View Tweets", slug: "/" },
-  { name: "Referral", slug: "/" },
-  { name: "Affiliate", slug: "/" },
-];
-export const supportLinks = [
-  { name: "Support Center", slug: "/" },
-  { name: "Submit an issue", slug: "/" },
-  { name: "Ask about a tweet", slug: "/" },
-  { name: "APIs", slug: "/" },
-];
-export const learnLinks = [
-  { name: "Learn to trade", slug: "/" },
-  { name: "Napcat Academy", slug: "/" },
-  { name: "Learn the Basics", slug: "/" },
-  { name: "Learn and earn", slug: "/" },
-  { name: "view trade impacts", slug: "/" },
+export const footerLinks = [
+  {
+    name: "About Us",
+    links: [
+      { name: "About", slug: "/" },
+      { name: "Careers", slug: "/" },
+      { name: "Business Contacts", slug: "/" },
+      { name: "Community", slug: "/" },
+      { name: "Legal", slug: "/" },
+      { name: "Terms of Service", slug: "/" },
+      { name: "Privacy", slug: "/" },
+      { name: "Risks", slug: "/" },
+      { name: "Announcements", slug: "/" },
+      { name: "Notices", slug: "/" },
+    ],
+  },
+  {
+    name: "Products",
+    links: [
+      { name: "Exchange", slug: "/" },
+      { name: "Academy", slug: "/" },
+      { name: "Napcat Live", slug: "/" },
+      { name: "Charity", slug: "/" },
+      { name: "Labs", slug: "/" },
+      { name: "Launchpad", slug: "/" },
+      { name: "Research", slug: "/" },
+    ],
+  },
+  {
+    name: "Service",
+    links: [
+      { name: "Downloads", slug: "/" },
+      { name: "Desktop Application", slug: "/" },
+      { name: "Buy Crypto", slug: "/" },
+      { name: "View Tweets", slug: "/" },
+      { name: "Referral", slug: "/" },
+      { name: "Affiliate", slug: "/" },
+    ],
+  },
+  {
+    name: "Support",
+    links: [
+      { name: "Support Center", slug: "/" },
+      { name: "Submit an issue", slug: "/" },
+      { name: "Ask about a tweet", slug: "/" },
+      { name: "APIs", slug: "/" },
+    ],
+  },
+  {
+    name: "Learn",
+    links: [
+      { name: "Learn to trade", slug: "/" },
+      { name: "Napcat Academy", slug: "/" },
+      { name: "Learn the Basics", slug: "/" },
+      { name: "Learn and earn", slug: "/" },
+      { name: "view trade impacts", slug: "/" },
+    ],
+  },
 ];
