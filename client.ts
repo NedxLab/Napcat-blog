4e18e610d005a86f29039f7383d1809b26ed9a61
NedxLab <odoc047@gmail.com>
2023-02-23 18:10:17 +0100

    env locals

--- client.ts
+++ client.ts
@@ -3,7 +3,7 @@
 import { createClient } from "@sanity/client";
 
 export default createClient({
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID, // you can find this in sanity.json
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // you can find this in sanity.json
   dataset: "production", // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
 });
