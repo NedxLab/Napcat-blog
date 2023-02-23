f1fb1cafe4fe2f6ae7ee25a0af6e4c19be464e29
NedxLab <odoc047@gmail.com>
2023-02-23 16:58:49 +0100

    any

--- utils/index.tsx
+++ utils/index.tsx
@@ -1,6 +1,6 @@
 import imageUrlBuilder from "@sanity/image-url";
 import client from "@/client";
 
-export default function urlFor(source) {
+export default function urlFor(source: any) {
   return imageUrlBuilder(client).image(source);
 }
